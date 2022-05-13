dd844df8aededc54f67000069a22a11b8ee32d87
Cristian Yovani Martinez Contreras <76701131+CrisM93@users.noreply.github.com>
2022-05-13 14:18:13 -0500

    Agregegue peticiones UsuairosId, y UsuariosAll
    Get por id de usuarios
    Get de todos los usuarios

--- src/main/java/mx/uam/tsis/proyecto/negocio/UsuarioService.java
+++ src/main/java/mx/uam/tsis/proyecto/negocio/UsuarioService.java
@@ -1,10 +1,17 @@
 package mx.uam.tsis.proyecto.negocio;
 
+import java.util.Iterator;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import mx.uam.tsis.proyecto.api.dto.EmpleadoDto;
+import mx.uam.tsis.proyecto.api.dto.ListaEmpleadosDto;
+import mx.uam.tsis.proyecto.api.dto.ListaUsuariosDto;
 import mx.uam.tsis.proyecto.api.dto.UsuarioDto;
 import mx.uam.tsis.proyecto.datos.UsuarioRepository;
+import mx.uam.tsis.proyecto.negocio.modelo.Empleado;
 import mx.uam.tsis.proyecto.negocio.modelo.Usuario;
 
 @Service
@@ -39,4 +46,43 @@ public class UsuarioService {
 		
 		return dto;
 	}
+	
+	public ListaUsuariosDto retrieveAll() {
+		Iterable <Usuario> listaRepo = usuarioRepository.findAll();
+		ListaUsuariosDto dto = new ListaUsuariosDto();
+		Usuario iteraUsuario;
+		UsuarioDto auxDto;
+		Iterator<Usuario> it = listaRepo.iterator();
+		while(it.hasNext()) {
+			iteraUsuario = it.next();
+			auxDto = UsuarioDto.builder()
+					.id(iteraUsuario.getId())
+					.nombre(iteraUsuario.getNombre())
+					.telefono(iteraUsuario.getTelefono())
+					.correo(iteraUsuario.getCorreo())
+					.ubicacion(iteraUsuario.getUbicacion())
+					.ubicacion(iteraUsuario.getUbicacion())
+					.contrasena(iteraUsuario.getContrasena())
+					.build();
+			dto.addListaUsuariosItem(auxDto);
+		}
+		return dto;
+	}
+public UsuarioDto getById(Integer id) {
+		
+		Optional <Usuario> usuarioOpt = usuarioRepository.findById(id);
+		UsuarioDto dto = UsuarioDto.builder()
+				.id(usuarioOpt.get().getId())
+				.nombre(usuarioOpt.get().getNombre())
+				.telefono(usuarioOpt.get().getTelefono())
+				.correo(usuarioOpt.get().getCorreo())
+				.ubicacion(usuarioOpt.get().getUbicacion())
+				.ubicacion(usuarioOpt.get().getUbicacion())
+				.contrasena(usuarioOpt.get().getContrasena())
+				.build();
+		return dto;
+	}
+	
+	
+	
 }
